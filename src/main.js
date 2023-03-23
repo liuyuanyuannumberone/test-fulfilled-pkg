9fbe9a0b569dd8688b152f42c078e351c4ed6427
charlesliuy <1445179059@qq.com>
2023-03-23 13:35:56 +0800

    init

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import TestSfcCli from '@charlesliuy/test-sfc-cli'
+Vue.use(TestSfcCli)
 
 Vue.config.productionTip = false
-
 new Vue({
   router,
   store,
